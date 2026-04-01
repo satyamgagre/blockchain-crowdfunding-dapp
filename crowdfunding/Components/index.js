7883de8aa07d9fa407ce40d76122e29ac1c43aa7
Satyam Gagare <satyamgagre4@gmail.com>
2026-04-01 16:41:29 +0530

    Create index.js

---
+++ crowdfunding/Components/index.js
@@ -0,0 +1,9 @@
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+import Logo from "./Logo";
+import Menu from "./Menu";
+import Card from "./Card";
+import Hero from "./Hero";
+import PopUp from "./PopUp";
+
+export { Footer, NavBar, Logo, Menu, Card, Hero, PopUp };
